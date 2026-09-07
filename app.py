b4464f043a99d3a8d457c0bdbb3a27a9bb3d299f
jospinoopino18-jpg <jospinoopino18@gmail.com>
2026-09-07 09:05:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-import telebot
-TOKEN = "8857935832:AAE8c7Xdqlb5o9vQmfSbpiH7anBMsLLSLts"
-bot = telebot.TeleBot(TOKEN)
+import os, threading
+from flask import Flask
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
-CANAL = "@ton_nom_de_canal" # ex: @crypto_douala
+TOKEN = os.environ.get("8857935832:AAE8c7Xdqlb5o9vQmfSbpiH7anBMsLLSLts")  # tu mettras ton token dans Render
 
-@bot.message_handler(func=lambda m: True)
-def repondre(message):
-    bot.reply_to(message, "Salut, j'ai bien reçu ton message !")
-    bot.send_message(CANAL, f"Nouveau message de {message.from_user.first_name}: {message.text}")
+app = Flask(__name__)
 
-bot.infinity_polling()
+@app.route("/")
+def health():
+    return "Bot en ligne !"
+
+@app.route("/health")
+def health2():
+    return "OK"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Salut Jordan ! Le bot crypto est en ligne 🚀 24/24 sur Render !")
+
+def run_bot():
+    application = Application.builder().token(TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+    application.run_polling()
+
+if __name__ == "__main__":
+    threading.Thread(target=run_bot, daemon=True).start()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
