c784adebc5424410b32399b7dd8d51dc0e6248e7
jospinoopino18-jpg <jospinoopino18@gmail.com>
2026-09-07 02:24:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-import requests, os, time, threading
+import requests, time, threading
 from flask import Flask
-TOKEN=os.environ.get("8606332059:AAFhaW3DocdsC-0byBHhkLfaTy-UhktOBTo")
-CHAT_ID=os.environ.get("7335134261")
-app=Flask(__name__)
-def bot_loop():
-    try:
-        requests.post(f"https://api.telegram.org/bot{TOKEN}/sendMessage",data={"chat_id":CHAT_ID,"text":"BOT ANALYSEUR RELANCE 🚀 Enfin!"},timeout=10)
-    except Exception as e:
-        print(e)
+
+TOKEN = "8606332059:AAFhaW3DocdsC-0byBHhkLfaTy-UhktOBTo"  # celui qui a marché pour le TEST
+CHAT_ID = "7335134261"
+
+app = Flask(__name__)
+
+def loop():
+    requests.post(f"https://api.telegram.org/bot{TOKEN}/sendMessage", data={"chat_id": CHAT_ID, "text": "BOT ENFIN EN LIGNE 🚀"})
     while True:
         time.sleep(60)
-threading.Thread(target=bot_loop,daemon=True).start()
+
+threading.Thread(target=loop, daemon=True).start()
+
 @app.route('/')
-def home(): return "BOT ACTIF"
-if __name__=='__main__':
-    app.run(host='0.0.0.0',port=int(os.environ.get("PORT",10000)))
+def home():
+    return "BOT OK"
+
+app.run(host='0.0.0.0', port=10000)
