3a7b9ac2d47f59f3192940f0530ecdd245e6ccb7
jospinoopino18-jpg <jospinoopino18@gmail.com>
2026-09-07 03:45:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import telebot
-TOKEN = "8857935832:AAGcA6N7JNzeKLW2cjL_RrcNx_gPTTUItKw"
+TOKEN = "8857935832:AAE8c7Xdqlb5o9vQmfSbpiH7anBMsLLSLts"
 bot = telebot.TeleBot(TOKEN)
 
 CANAL = "@ton_nom_de_canal" # ex: @crypto_douala
