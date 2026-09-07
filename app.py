aa5882e368965497b7cc2f7647df7fe61e85ba04
jospinoopino18-jpg <jospinoopino18@gmail.com>
2026-09-07 01:31:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import requests, os
-TOKEN = os.environ.get("8606332059:AAFhaW3DocdsC-0byBHhkLfaTy-UhktOBTo")
-CHAT_ID = os.environ.get("7335134261")
+TOKEN = os.environ.get"8606332059:AAFhaW3DocdsC-0byBHhkLfaTy-UhktOBTo"
+CHAT_ID = os.environ.get"7335134261"
 print(f"TOKEN existe? {bool(TOKEN)}")
 print(f"CHAT_ID: {CHAT_ID}")
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
